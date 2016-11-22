d2edbaa6a23181537804e040074358f65e6e1a3f
山能敦志 <AtsushiYamanou@yamanouatsushi-no-MacBook-Pro.local>
2016-11-22 15:40:48 +0900

    dive17

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
 
-    # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
         format.json { render :show, status: :created, location: @comment }
-        # JS形式でレスポンスを返します。
         format.js { render :index }
       else
         format.html { render :new }
@@ -21,9 +18,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-
     @blog = @comment.blog
-    
     redirect_to blog_path(@blog), notice: "コメントを削除しました！"
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,25 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   mount_uploader :avatar, AvatarUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
+
   has_many :blogs, dependent: :destroy
-
   has_many :comments, dependent: :destroy
-
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
-
   has_many :followed_users, through: :relationships, source: :followed
   has_many :followers, through: :reverse_relationships, source: :follower
-
   has_many :tasks, dependent: :destroy
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
-
     unless user
       user = User.new(
           name: auth.extra.raw_info.name,
           provider: auth.provider,
           uid: auth.uid,
           email: auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
-          #email: auth.info.email ||= "",
           image_url: auth.info.image,
           password: Devise.friendly_token[0, 20]
       )
@@ -38,7 +31,6 @@ class User < ActiveRecord::Base
 
   def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
-
     unless user
       user = User.new(
           name:     auth.info.nickname,
@@ -78,5 +70,4 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
-
 end
