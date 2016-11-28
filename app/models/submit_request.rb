cf8e48b01a8df03d6adc9e70e547332360cbd2a4
山能敦志 <AtsushiYamanou@yamanouatsushi-no-MacBook-Pro.local>
2016-11-28 14:38:47 +0900

    DIVE18課題 修正 タスク却下・承認エラー解消、タスク一覧画面表示

--- app/models/submit_request.rb
+++ app/models/submit_request.rb
@@ -4,7 +4,6 @@ class SubmitRequest < ActiveRecord::Base
   belongs_to :request_user, class_name: 'User', foreign_key: 'request_user_id'
   validates :user_id, :task_id, :request_user_id, presence: true
   validate :no_repeated_request, on: :create
-  enum status: {依頼中:1, 承認済み:2, 依頼を却下:9}
 
   private
 
